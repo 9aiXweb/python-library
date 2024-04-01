e32a556acc796831600f64e46f81e324b9adc34e
digivcam24 <vimeanseththorngstf@gmail.com>
2024-04-01 10:47:44 +0900

    ...

--- sample.py
+++
@@ -1,9 +0,0 @@
-import getAbstractFromWeb as abstract
-    
-fetching_html_text = abstract.myrequests("https://arxiv.org/abs/1706.03762")
-
-text = abstract.html2text(fetching_html_text)
-
-abstract_content = abstract.abstract_content(text)
-
-print(abstract_content)
